b5beac91776c1223649b3cb5003bc6715ebeee7d
Mason Davis <mcd@udel.edu>
2024-09-25 11:22:45 -0400

    Solved state

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [revealed, setRevealed] = useState<boolean>(false);
+    function flipRevealed(): void {
+        setRevealed(!revealed);
+    }
+    return (
+        <div>
+            <Button onClick={flipRevealed}>Reveal Answer</Button>
+            {revealed && <div>42</div>}
+        </div>
+    );
 }
